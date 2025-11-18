80ec232992cf5644365f62f78d15d365910b383d
Daniel Labrador Benito <danielabrador47@gmail.com>
2025-11-18 19:25:37 +0100

    corriguiendo workdlows de deploy e integracion

--- scripts/register_model.py
+++ scripts/register_model.py
@@ -1,9 +1,9 @@
 import os, mlflow
 from mlflow.tracking import MlflowClient
 
-mlflow.set_tracking_uri(os.getenv("MLFLOW_URL"))  # antes estaba hardcodeado
+mlflow.set_tracking_uri(os.getenv("MLFLOW_URL"))
 
-# Obtener run_id desde una variable de entorno RUN_ID
+#obtenogo run_id desde la varibale de entorno RUN_ID
 run_id = os.getenv("RUN_ID")
 if not run_id:
     with open("run_id.txt") as f:
@@ -13,6 +13,6 @@ client = MlflowClient()
 model_name  = os.getenv("MODEL_NAME", "no_name")
 result = mlflow.register_model(model_uri=f"runs:/{run_id}/models/model.pkl", name=model_name)
 
-# Transicionar a Staging y establecer alias
+#transiciono a Staging y establecer alias
 client.transition_model_version_stage(name=model_name, version=result.version, stage="Staging")
 client.set_registered_model_alias(model_name, os.getenv("MODEL_ALIAS", "champion"), result.version)
